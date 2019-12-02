71aa03313a5b009daa8b7ec9e28782928921d972
Nikita Petrenko <npetrenko97@gmail.com>
2019-12-03 00:45:22 +0300

    better run script

---
+++ README.md
@@ -0,0 +1,2 @@
+Example usage:
+`./run_bench.sh copy_or_ref_bench/`
--- run_bench.sh
+++ run_bench.sh
@@ -2,9 +2,12 @@
 
 mkdir release
 
-BENCH_NAME=$1
+BENCH_NAME=${1%/}
+START_DIR=`pwd`
 
 cd release && \
     cmake -DCMAKE_BUILD_TYPE=RELEASE .. && \
     cd $BENCH_NAME &&
-    make && ./$BENCH_NAME && cd .. && cd ..
+    make && ./$BENCH_NAME
+
+cd $START_DIR
