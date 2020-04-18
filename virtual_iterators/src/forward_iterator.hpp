be4c17849d712c4e522d3c53b4961b8b93dca40e
Nikita Petrenko <npetrenko97@gmail.com>
2020-04-18 17:54:36 +0300

    split static-dynamic manager

--- virtual_iterators/src/forward_iterator.hpp
+++ virtual_iterators/src/forward_iterator.hpp
@@ -67,71 +67,16 @@ public:
     }
 
 private:
-    class AbstractManager {
-    public:
-        template <class Iterator>
-        static AbstractManager CreateManager() {
-            return AbstractManager{[](const OpCode code, std::any& iterator, void* payload) -> uint64_t {
-                auto* iter = AssertAnyCast<Iterator>(&iterator);
-                switch (code) {
-                    case OpCode::Increment: {
-                        assert(!payload);
-                        ++(*iter);
-                        // placeholder value
-                        return 2;
-                    }
-                    case OpCode::IsEqual: {
-                        auto* other = AssertAnyCast<const Iterator>(static_cast<IsEqualPayload*>(payload));
-                        return (*other) == (*iter);
-                    }
-                    case OpCode::Clone: {
-                        auto* typedPayload = static_cast<ClonePayload*>(payload);
-                        *typedPayload = *iter;
-                        return 3;
-                    }
-                    case OpCode::Deref: {
-                        assert(!payload);
-                        pointer ptr = &**iter;
-                        return reinterpret_cast<uint64_t>(ptr);
-                    }
-                }
-            }};
-        }
+    enum class OpCode {
+        Deref,
+        Increment,
+        IsEqual,
+        Clone,
 
+        Dynamic,
+    };
 
-        void Increment(std::any& iterator) const {
-            op_func_(OpCode::Increment, iterator, nullptr);
-        }
-
-        bool Equal(const std::any& first, const std::any& second) const {
-            return op_func_(OpCode::IsEqual,
-                            const_cast<std::any&>(first),
-                            const_cast<std::any*>(&second));
-        }
-
-        std::any Clone(const std::any& iterator) const {
-            std::any result;
-            op_func_(OpCode::Clone, const_cast<std::any&>(iterator), &result);
-            return result;
-        }
-
-        reference Deref(const std::any& iterator) const {
-            uint64_t addr = op_func_(OpCode::Deref, const_cast<std::any&>(iterator), nullptr);
-            auto* ptr = reinterpret_cast<pointer>(addr);
-            return *ptr;
-        }
-
-    private:
-        enum class OpCode {
-            Increment,
-            IsEqual,
-            Clone,
-            Deref,
-        };
-        using IncrementPayload = void;
-        using IsEqualPayload = const std::any;
-        using ClonePayload = std::any;
-
+    struct ManagerState {
         // returns:
         // 2 if code == Increment (placeholder value)
         // 3 if code == Clone (placeholder value)
@@ -139,12 +84,95 @@ private:
         // address of the element if code == Deref
         using OpFunc = uint64_t (*)(OpCode code, std::any& iterator, void* payload);
 
-        explicit AbstractManager(OpFunc func) noexcept
-            : op_func_{func}
+        OpFunc deref_func_;
+        OpFunc inc_func_;
+        OpFunc iseq_func_;
+        OpFunc dyn_func_;
+    };
+
+    class AbstractManager : ManagerState {
+        using Base = ManagerState;
+
+    public:
+        template <class Iterator>
+        static AbstractManager CreateManager() {
+            return AbstractManager{
+                Manage<Iterator, OpCode::Deref>,
+                Manage<Iterator, OpCode::Increment>,
+                Manage<Iterator, OpCode::IsEqual>,
+                Manage<Iterator, OpCode::Dynamic>,
+            };
+        }
+        __attribute__((always_inline))
+        void Increment(std::any& iterator) const {
+            this->inc_func_(OpCode::Increment, iterator, nullptr);
+        }
+
+        __attribute__((always_inline))
+        bool Equal(const std::any& first, const std::any& second) const {
+            return this->iseq_func_(OpCode::IsEqual,
+                            const_cast<std::any&>(first),
+                            const_cast<std::any*>(&second));
+        }
+
+        std::any Clone(const std::any& iterator) const {
+            std::any result;
+            this->dyn_func_(OpCode::Clone, const_cast<std::any&>(iterator), &result);
+            return result;
+        }
+
+        __attribute__((always_inline))
+        reference Deref(const std::any& iterator) const {
+            uint64_t addr = this->deref_func_(OpCode::Deref, const_cast<std::any&>(iterator), nullptr);
+            auto* ptr = reinterpret_cast<pointer>(addr);
+            return *ptr;
+        }
+
+    private:
+        using IncrementPayload = void;
+        using IsEqualPayload = const std::any;
+        using ClonePayload = std::any;
+
+    private:
+        template <class... Args>
+        explicit AbstractManager(Args&&... args) noexcept
+            : Base{std::forward<Args>(args)...}
         {
         }
 
-        OpFunc op_func_;
+        template <class Iterator, OpCode ccode = OpCode::Dynamic>
+        static uint64_t Manage(const OpCode dcode, std::any& iterator, void* payload) {
+            auto* const iter = AssertAnyCast<Iterator>(&iterator);
+            const auto code = ccode == OpCode::Dynamic ? dcode : ccode;
+
+            switch (code) {
+                case OpCode::Deref: {
+                    assert(!payload);
+                    pointer ptr = &**iter;
+                    return reinterpret_cast<uint64_t>(ptr);
+                }
+                case OpCode::Increment: {
+                    assert(!payload);
+                    ++(*iter);
+                    // placeholder value
+                    return 2;
+                }
+                case OpCode::IsEqual: {
+                    auto* other = AssertAnyCast<const Iterator>(static_cast<IsEqualPayload*>(payload));
+                    return (*other) == (*iter);
+                }
+                case OpCode::Clone: {
+                    auto* typedPayload = static_cast<ClonePayload*>(payload);
+                    *typedPayload = *iter;
+                    return 3;
+                }
+
+                case OpCode::Dynamic:
+                    std::terminate();
+            }
+            std::terminate();
+        }
+    private:
     };
 
     std::any obj_;
